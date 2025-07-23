bdebe48bb769c6894760ca7342fa2c097abfddcd
Noam Norman <normannoam21@gmail.com>
2025-07-23 13:41:00 +0300

    refactor: update import statements and improve root rendering in main.tsx

--- src/components/TimeTracker.tsx
+++ src/components/TimeTracker.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import {
   Dialog,
-  DialogTrigger,
   DialogContent,
   DialogHeader,
   DialogFooter,
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
